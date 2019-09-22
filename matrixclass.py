922138c08cafcbc10d896d009feb114f46d48587
uliana2006eskova <45653414+uliana2006eskova@users.noreply.github.com>
2019-09-22 16:16:53 +0300

    Update matrixclass.py

--- matrixclass.py
+++ matrixclass.py
@@ -2,16 +2,14 @@ import copy
 class MyMatrix:
     def __init__(self, data: list):
         self.matrix = copy.deepcopy(data)
-        self.width = 0
-        self.height = 0
 
     def size(self) -> tuple:
         if self.matrix == []:
             s = (0, 0)
         else:
-            self.height = len(self.matrix)
-            self.weight = len(self.matrix[0])
-            s = (self.height, self.weight)
+            height = len(self.matrix)
+            weight = len(self.matrix[0])
+            s = (height, weight)
         return s
 
     def __repr__(self):
@@ -25,31 +23,41 @@ class MyMatrix:
                         s += str(line[i]) + ' '
             return s[1::]
 
+    """def __get_data(self, nowlist):
+        for i in range (int(self.size()[0])):
+            rowList = []
+            for j in range (int(self.size()[1])):
+                if nowlist[j] not in matrix:
+                    rowList.append(nowlist[j])
+        matrix.append(rowList)
+        return matrix"""
+
     def flip_up_down(self):
         self.matrix = self.matrix[::-1]
-        return self.matrix
+        return self
 
     def flip_left_right(self):
         flipped_matrix = []
         for row in self.matrix:
             flipped_matrix.append(row[::-1])
         self.matrix = copy.deepcopy(flipped_matrix)
-        return self.matrix
+        return self
 
     def flipped_up_down(self):
-        flipped_matrix = MyMatrix(copy.deepcopy(self.matrix))
-        return flipped_matrix.flip_up_down()
+        flipped_matrix = MyMatrix(self.matrix)
+        flipped_matrix.flip_up_down()
+        return flipped_matrix.matrix
 
     def flipped_left_right(self):
-        flipped_matrix = MyMatrix(copy.deepcopy(self.matrix))
+        flipped_matrix = MyMatrix(self.matrix)
         flipped_matrix = flipped_matrix.flip_left_right()
-        return flipped_matrix
+        return flipped_matrix.matrix
 
     def transpose(self):
         self.matrix = [[row[i] for row in self.matrix] for i in range(len(self.matrix[0]))]
-        return self.matrix
+        return self
 
     def transposed(self):
-        flipped_matrix = MyMatrix(copy.deepcopy(self.matrix))
+        flipped_matrix = MyMatrix(self.matrix)
         flipped_matrix = flipped_matrix.transpose()
-        return flipped_matrix
+        return flipped_matrix.matrix
